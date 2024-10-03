37cde1de638d7a5c3270291b9449fc7ebda6567a
daixin <daixin016@ke.com>
2024-10-03 18:29:02 +0200

    1

--- main.py
+++ main.py
@@ -67,7 +67,8 @@ def recommend(cir_df, rank=25, endpoint="api/v3/ticker/24hr"):
 
         # 过滤出包含 "USDT" 的币种
         usdt_symbols_rise = [[token['symbol']] for token in sorted_res_rise if
-                             token['symbol'].endswith('USDT')]
+                             token['symbol'].endswith('USDT') and 'USDC' not in token['symbol'] and 'FDUSD' not in
+                             token['symbol'] and token['count'] != 0]
         # 筛选出前20
         usdt_symbols_rise = usdt_symbols_rise[:rank]
         # 增加额外的币种
@@ -500,7 +501,8 @@ def scan_big_order(record, endpoint='api/v3/ticker/24hr', rank=15, add=None):
 
         # 过滤出包含 "USDT" 的币种
         usdt_symbols_rise = [token['symbol'] for token in sorted_res_rise if
-                             token['symbol'].endswith("USDT")]
+                             token['symbol'].endswith("USDT") and 'USDC' not in token['symbol'] and 'FDUSD' not in
+                             token['symbol'] and token['count'] != 0]
         # 筛选出前15
         usdt_symbols_rise = usdt_symbols_rise[:rank]
         # 增加额外的币种
