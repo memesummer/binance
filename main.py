78e0b220f66511324efadc30c9ca3ab5ef779d08
daixin <daixin016@ke.com>
2024-11-14 22:33:45 +0800

    debug

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def recommend(cir_df, rank=25, endpoint="api/v3/ticker/24hr"):
         # 按涨幅排序
         sorted_res_rise = sorted(res, key=lambda x: float(x['priceChangePercent']), reverse=True)
 
-        fil_str_list = ['USDC', 'FDUSD', 'TUSD', 'USDP', 'EUR']
+        fil_str_list = ['USDC', 'FDUSD', 'TUSDUSDT', 'USDP', 'EUR']
 
         # 过滤出包含 "USDT" 的币种
         usdt_symbols_rise = [token['symbol'] for token in sorted_res_rise if
@@ -516,7 +516,7 @@ def scan_big_order(record, endpoint='api/v3/ticker/24hr', rank=15, add=None):
         # 按涨幅排序
         sorted_res_rise = sorted(res, key=lambda x: float(x['priceChangePercent']), reverse=True)
 
-        fil_str_list = ['USDC', 'FDUSD', 'TUSD', 'USDP', 'EUR']
+        fil_str_list = ['USDC', 'FDUSD', 'TUSDUSDT', 'USDP', 'EUR']
 
         # 过滤出包含 "USDT" 的币种
         usdt_symbols_rise = [token['symbol'] for token in sorted_res_rise if
