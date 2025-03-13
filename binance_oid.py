f5562f2ac0bbcdf8eed4fb0f00cbedce0e349139
daixin <daixin016@ke.com>
2025-03-13 12:15:22 +0800

    oid debug

--- binance_oid.py
+++ binance_oid.py
@@ -49,7 +49,7 @@ def run_task():
             continue
         diff_ratio = l[2]
         if diff_ratio >= 3:
-            res += f"🌀*{l[0]}*近15分钟净持仓增加`{format_number(float(l[1]))}`｜`{str(l[2])}%`｜`{str(l[3])}%`\n"
+            res += f"🌀*{l[0][4:] if l[0].startswith('1000') else l[0]}*近15分钟净持仓增加`{format_number(float(l[1]))}`｜`{str(l[2])}%`｜`{str(l[3])}%`\n"
             binance_his.add(''.join(map(str, l)))
         else:
             continue
