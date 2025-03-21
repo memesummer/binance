c9d60f09701350e1492c0b49ab49ed0e336f832e
daixin <daixin016@ke.com>
2025-03-21 11:52:56 +0800

    停止2个任务

--- restart_bots.sh
+++ restart_bots.sh
@@ -40,8 +40,8 @@ echo "正在重新启动程序..."
 /usr/bin/nohup /opt/anaconda3/envs/bot/bin/python -u /opt/daixin/binance/binance_recommed.py >> push.log 2>&1 &
 /usr/bin/nohup /opt/anaconda3/envs/bot/bin/python -u /opt/daixin/binance/binance_oid.py >> oid.log 2>&1 &
 /usr/bin/nohup /opt/anaconda3/envs/bot/bin/python -u /opt/daixin/binance/bitget.py >> bitget.log 2>&1 &
-/usr/bin/nohup /opt/anaconda3/envs/bot/bin/python -u /opt/daixin/binance/lbank.py >> lbank.log 2>&1 &
-/usr/bin/nohup /opt/anaconda3/envs/bot/bin/python -u /opt/daixin/binance/solana.py >> solana.log 2>&1 &
+#/usr/bin/nohup /opt/anaconda3/envs/bot/bin/python -u /opt/daixin/binance/lbank.py >> lbank.log 2>&1 &
+#/usr/bin/nohup /opt/anaconda3/envs/bot/bin/python -u /opt/daixin/binance/solana.py >> solana.log 2>&1 &
 
 
 echo "程序已重新启动"
