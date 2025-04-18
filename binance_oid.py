f28e8063625b1f5ea5cbfa6204714bf52b016ac1
daixin <daixin016@ke.com>
2025-04-18 13:40:33 +0800

    格式调整

--- binance_oid.py
+++ binance_oid.py
@@ -49,7 +49,7 @@ def run_task():
             continue
         diff_ratio = l[2]
         if diff_ratio >= 100:
-            res += f"🌀*{l[0][4:] if l[0].startswith('1000') else l[0]}*近15分钟净持仓量增加`{format_number(float(l[1]))}`｜`{str(l[2])}%`｜`{str(l[3])}%`\n"
+            res += f"🐂🌋*{l[0][4:] if l[0].startswith('1000') else l[0]}*主力多头扩张`{format_number(float(l[1]))}`｜`{str(l[2])}%`｜`{str(l[3])}%`\n"
             binance_his.add(''.join(map(str, l)))
         else:
             continue
@@ -59,7 +59,7 @@ def run_task():
             continue
         diff_ratio = l[5]
         if diff_ratio >= 3:
-            res += f"🌪️*{l[0][4:] if l[0].startswith('1000') else l[0]}*近15分钟持仓量增加`{format_number(float(l[4]))}`｜`{str(l[5])}%`｜`{str(l[3])}%`\n"
+            res += f"🧲🔼*{l[0][4:] if l[0].startswith('1000') else l[0]}*市场增量`{format_number(float(l[4]))}`｜`{str(l[5])}%`｜`{str(l[3])}%`\n"
             binance_his.add(''.join(map(str, l)))
         else:
             continue
